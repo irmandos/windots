4815a99939aabe15177426200c31196b12b3e41f
Ricardo Botha <irmandos@gmail.com>
2024-06-19 15:40:29 +0200

    Getting Started, nothing much yet.

---
+++ profile.ps1
@@ -0,0 +1,188 @@
+###############################
+###   MY CUSTOM FUNCTIONS   ###
+###############################
+
+# Define the list of modules to install
+$myModules = @(
+    @{moduleName = "Microsoft.WinGet.Client"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSWriteColor"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSReadLine"; installOptions = "-AcceptLicense -AllowPrerelease -Force -SkipPublisherCheck"},
+    @{moduleName = "posh-git"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "Terminal-Icons"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "M365PSProfile"; installOptions = "-AcceptLicense -Force && Install-PSResource -Name M365PSProfile"},
+    @{moduleName = "Az.Accounts"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "Az.Tools.Predictor"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSWindowsUpdate"; installOptions = "-AcceptLicense -Force"}
+)
+
+# Define the list of applications to install
+$AppList = @(
+    @{ Command = 'oh-my-posh'; WingetName = 'JanDeDobbeleer.OhMyPosh'; OptionalStrings = '' },
+    @{ Command = 'git'; WingetName = 'git.git'; OptionalStrings = '' },
+    @{ Command = 'fastfetch'; WingetName = 'Fastfetch-cli.Fastfetch'; OptionalStrings = '' },
+    @{ Command = 'onefetch'; WingetName = 'o2sh.onefetch'; OptionalStrings = '' }
+)
+
+# Function to install modules asynchronously
+function Install-Modules {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$Modules
+    )
+
+    $totalModules = $Modules.Count
+    $completedModules = 0
+
+    $jobs = @()
+    foreach ($module in $Modules) {
+        $moduleName = $module.moduleName
+        $installOptions = $module.installOptions
+
+        $jobs += Start-Job -Name "Install-$moduleName" -ScriptBlock {
+            if (-not (Get-Module -ListAvailable -Name $using:moduleName)) {
+                Install-Module -Name $using:moduleName -Scope CurrentUser $using:installOptions
+                Import-Module $using:moduleName
+            }
+            Import-Module $using:moduleName
+        }
+    }
+
+    # Display progress bar
+    do {
+        $completedModules = ($jobs | Where-Object { $_.State -eq 'Completed' }).Count
+        Write-Progress -Activity "Installing Modules" -Status "Progress" -PercentComplete ($completedModules / $totalModules * 100)
+        Start-Sleep -Seconds 1
+    } while ($completedModules -lt $totalModules)
+
+    # Wait for all jobs to complete and check for any issues
+    $jobs | Wait-Job | Out-Null
+    $failedJobs = $jobs | Where-Object { $_.State -ne 'Completed' }
+    if ($failedJobs) {
+        Write-Host "Some module installations failed:"
+        $failedJobs | ForEach-Object { Receive-Job -Job $_ }
+    }
+}
+
+# Function to install applications asynchronously
+function Install-Applications {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$AppList
+    )
+
+    $totalApps = $AppList.Count
+    $completedApps = 0
+
+    $jobs = @()
+    foreach ($app in $AppList) {
+        $jobs += Start-Job -Name "Install-$($app.Command)" -ScriptBlock {
+            if (-not (Get-Command -Name $using:app.Command -ErrorAction SilentlyContinue)) {
+                Write-Verbose "Installing $using:app.WingetName..."
+                Install-WinGetPackage $using:app.WingetName
+            }
+        }
+    }
+
+    # Display progress bar
+    do {
+        $completedApps = ($jobs | Where-Object { $_.State -eq 'Completed' }).Count
+        Write-Progress -Activity "Installing Applications" -Status "Progress" -PercentComplete ($completedApps / $totalApps * 100)
+        Start-Sleep -Seconds 1
+    } while ($completedApps -lt $totalApps)
+
+    # Wait for all jobs to complete and check for any issues
+    $jobs | Wait-Job | Out-Null
+    $failedJobs = $jobs | Where-Object { $_.State -ne 'Completed' }
+    if ($failedJobs) {
+        Write-Host "Some application installations failed:"
+        $failedJobs | ForEach-Object { Receive-Job -Job $_ }
+    }
+}
+
+# Function for Oh-My-Posh customisations
+function Set-OhMyPosh {
+    if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) {
+        if (!(Test-Path "$env:LOCALAPPDATA\Microsoft\Windows\Fonts\CaskaydiaCove*")) {
+            oh-my-posh font install CascadiaCode --user
+        }
+        oh-my-posh init pwsh --config "$env:OneDriveConsumer\Documents\PowerShell\oh-my-posh-theme.json" | Invoke-Expression
+    } else {
+        Write-Host "Oh-My-Posh was not found on this system"
+    }
+}
+
+# Function for PSReadLine customisations
+function Set-PSReadLine {
+    if (Get-Command "Set-PSReadLineOption" -ErrorAction SilentlyContinue) {
+        Set-PSReadLineOption -PredictionViewStyle ListView
+        Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+        Set-PSReadLineOption -HistoryNoDuplicates
+        Set-PSReadLineKeyHandler -Key Tab -Function Complete
+    }
+}
+
+# Function to upgrade WinGet packages
+function wingetUpgrade {if (Get-Command "winget" -ErrorAction SilentlyContinue) {
+        winget upgrade --accept-source-agreements --accept-package-agreements --include-pinned --all
+    } else {
+        Write-Host "Oh no, what have you done, WinGet is missing!"
+    }
+}
+
+# Function to update PowerShell modules
+function updateModules {if (Get-Command "Install-M365Module" -ErrorAction SilentlyContinue) {
+        Write-Host "Checking WinGet..."
+        Install-M365Module -Modules $myModules.moduleName
+    } else {
+        Write-Host "Oh no, you do not have M365 module installed!"
+    }
+}
+
+# Function to check Windows Update status
+function windowsUpdateCheck {
+    Get-WULastResults | Select-Object LastSearchSuccessDate, LastInstallationSuccessDate
+}
+
+# Function to perform all updates
+function update {
+    updateModules
+    wingetUpgrade
+    #windowsUpdateCheck
+}
+
+# Show fastfetch in terminal or if not installed get powerfetch from online script
+function Show-fastfetch {
+    if (Get-Command "fastfetch" -ErrorAction SilentlyContinue) {
+        fastfetch -c paleofetch
+    } else {
+        Invoke-Expression (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/jantari/powerfetch/master/powerfetch.ps1')
+    }
+}
+
+#########################################
+###   SHELL CUSTOMISATION FUNCTIONS   ###
+#########################################
+if ([Environment]::GetCommandLineArgs().Contains("-NonInteractive") -or [Environment]::GetCommandLineArgs().Contains("-CustomPipeName")) {
+    Install-Modules -Modules $myModules
+    return
+}
+
+if ($Host.Name -eq 'Visual Studio Code Host') {
+    Install-Modules -Modules $myModules
+	Customise-OhMyPosh 
+	Customise-PSReadLine
+    return
+}
+
+# Start Update-Help to run in the background to ensure helpfiles are always up to date
+# Update-Help will only run once per day and will require -Force to override this behaviour
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/update-help?view=powershell-7.4
+Start-Process pwsh -WindowStyle Hidden -ArgumentList "-NoProfile -Command Update-Help -Scope CurrentUser"
+
+Show-fastfetch
+Install-Modules -Modules $myModules
+Install-Applications -AppList $AppList
+Set-OhMyPosh
+Set-PSReadLine
---
+++ setup.ps1
@@ -0,0 +1,168 @@
+# Setup script for new windows machine
+Requires -RunAsAdministrator
+
+# Set working directory
+Set-Location $PSScriptRoot
+[Environment]::CurrentDirectory = $PSScriptRoot
+
+# Set Paths
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+$OneApps = "$Env:OneDriveConsumer\Apps\"
+
+# Linked Files (Destination => Source)
+$symlinks = @{
+    $PROFILE.CurrentUserAllHosts                                                                         = ".\profile.ps1"
+    "$Env:UserProfile\.gitconfig"                                                                        = "$OneApps\Git\.gitconfig"
+    "$Env:UserProfile\.ssh"                                                                              = "$OneApps\SSH"
+    "$Env:LocalAppData\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"        = "$OneApps\WindowsTerminal\settings.json"
+    "$Env:LocalAppData\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" = "$OneApps\WindowsTerminalBeta\settings.json"
+    
+}
+
+# Define the list of modules to  with additional options
+$myModules = @(
+    @{moduleName = "Microsoft.WinGet.Client"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSWriteColor"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSReadLine"; installOptions = "-AcceptLicense -AllowPrerelease -Force -SkipPublisherCheck"},
+    @{moduleName = "posh-git"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "Terminal-Icons"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "M365PSProfile"; installOptions = "-AcceptLicense -Force && Install-PSResource -Name M365PSProfile"},
+    @{moduleName = "Az.Accounts"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "Az.Tools.Predictor"; installOptions = "-AcceptLicense -Force"},
+    @{moduleName = "PSWindowsUpdate"; installOptions = "-AcceptLicense -Force"}
+)
+
+# Define the list of winget applications to install with additional options
+$AppList = @(
+    @{ Command = ''; WingetName = 'Microsoft.WindowsTerminal.Preview'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.DotNet.Runtime.6 '; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.VCRedist.2013.x86'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.VCRedist.2013.x64'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.VCRedist.2015+.x86'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.VCRedist.2015+.x64'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Cloudflare.Warp'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Cloudflare.cloudflared'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Windscribe.Windscribe'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Bitwarden.Bitwarden'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Resilio.ResilioSync'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'GnuPG.GnuPG'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'GnuPG.Gpg4win'; OptionalStrings = '' },
+    @{ Command = 'git'; WingetName = 'git.git'; OptionalStrings = '' },
+    @{ Command = 'oh-my-posh'; WingetName = 'JanDeDobbeleer.OhMyPosh'; OptionalStrings = '' },
+    @{ Command = 'fastfetch'; WingetName = 'Fastfetch-cli.Fastfetch'; OptionalStrings = '' },
+    @{ Command = 'onefetch'; WingetName = 'o2sh.onefetch'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Notepad++.Notepad++'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = '7zip.7zip'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'CodecGuide.K-LiteCodecPack.Full'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Valve.Steam'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'EpicGames.EpicGamesLauncher'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Ubisoft.Connect'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Microsoft.VisualStudioCode'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'namazso.OpenHashTab'; OptionalStrings = '' },
+    @{ Command = ''; WingetName = 'Discord.Discord'; OptionalStrings = '' }
+)
+
+
+# Function to install modules asynchronously
+function Install-Modules {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$Modules
+    )
+
+    $totalModules = $Modules.Count
+    $completedModules = 0
+
+    $jobs = @()
+    foreach ($module in $Modules) {
+        $moduleName = $module.moduleName
+        $installOptions = $module.installOptions
+
+        $jobs += Start-Job -Name "Install-$moduleName" -ScriptBlock {
+            if (-not (Get-Module -ListAvailable -Name $using:moduleName)) {
+                Install-Module -Name $using:moduleName -Scope CurrentUser $using:installOptions
+                Import-Module $using:moduleName
+            }
+            Import-Module $using:moduleName
+        }
+    }
+
+    # Display progress bar
+    do {
+        $completedModules = ($jobs | Where-Object { $_.State -eq 'Completed' }).Count
+        Write-Progress -Activity "Installing Modules" -Status "Progress" -PercentComplete ($completedModules / $totalModules * 100)
+        Start-Sleep -Seconds 1
+    } while ($completedModules -lt $totalModules)
+
+    # Wait for all jobs to complete and check for any issues
+    $jobs | Wait-Job | Out-Null
+    $failedJobs = $jobs | Where-Object { $_.State -ne 'Completed' }
+    if ($failedJobs) {
+        Write-Host "Some module installations failed:"
+        $failedJobs | ForEach-Object { Receive-Job -Job $_ }
+    }
+}
+
+# Function to install applications asynchronously
+function Install-Applications {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$AppList
+    )
+
+    $totalApps = $AppList.Count
+    $completedApps = 0
+
+    $jobs = @()
+    foreach ($app in $AppList) {
+        $jobs += Start-Job -Name "Install-$($app.WingetName)" -ScriptBlock {
+            if (-not (Get-WinGetPackage $using:app.WingetName -ErrorAction SilentlyContinue)) {
+                Write-Verbose "Installing $using:app.WingetName..."
+                Install-WinGetPackage $using:app.WingetName $using:app.OptionalStrings
+            }
+        }
+    }
+
+    # Display progress bar
+    do {
+        $completedApps = ($jobs | Where-Object { $_.State -eq 'Completed' }).Count
+        Write-Progress -Activity "Installing Applications" -Status "Progress" -PercentComplete ($completedApps / $totalApps * 100)
+        Start-Sleep -Seconds 1
+    } while ($completedApps -lt $totalApps)
+
+    # Wait for all jobs to complete and check for any issues
+    $jobs | Wait-Job | Out-Null
+    $failedJobs = $jobs | Where-Object { $_.State -ne 'Completed' }
+    if ($failedJobs) {
+        Write-Host "Some application installations failed:"
+        $failedJobs | ForEach-Object { Receive-Job -Job $_ }
+    }
+}
+
+# Function to create symbolic links
+function New-SymbolicLinks {
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Hashtable]$Symlinks
+    )
+
+    Write-Host "Creating Symbolic Links..."
+
+    foreach ($symlink in $Symlinks.GetEnumerator()) {
+        $existingItem = Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue
+        if ($existingItem) {
+            Remove-Item -Path $symlink.Key -Force -Recurse -ErrorAction SilentlyContinue
+        }
+
+        $targetPath = Resolve-Path $symlink.Value
+        New-Item -ItemType SymbolicLink -Path $symlink.Key -Target $targetPath -Force | Out-Null
+    }
+}
+
+# Setup Actions
+Install-Modules -Modules $myModules
+Install-Applications -AppList $AppList
+Create-SymbolicLinks -Symlinks $symlinks
